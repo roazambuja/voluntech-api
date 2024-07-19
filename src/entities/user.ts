c94251799fae989f223aefbdd02b29bb5888ebd6
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-18 23:55:13 -0300

    feat: rename entitiy and model and added picture attribute

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,8 +1,13 @@
 import { ObjectId } from "mongoose";
 
-export interface Users {
+export interface User {
   _id?: ObjectId;
   name: string;
   email: string;
   password: string;
+  role: string;
+  profilePicture?: {
+    filePath: string;
+    publicId: string;
+  };
 }
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,18 +1,22 @@
 import mongoose from "mongoose";
-import { Users } from "../entities/user";
+import { User } from "../entities/user";
 
-const UserSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema<User>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    profilePicture: {
+      filePath: String,
+      publicId: String,
+    },
     role: { type: String, required: true },
   },
   { timestamps: { createdAt: "created_at", updatedAt: "updated_at" }, discriminatorKey: "role" }
 );
 
-UserSchema.index({ email: 1 }, { unique: true });
+userSchema.index({ email: 1 }, { unique: true });
 
-const UserModel = mongoose.model<Users>("User", UserSchema);
+const UserModel = mongoose.model<User>("User", userSchema);
 
 export default UserModel;
