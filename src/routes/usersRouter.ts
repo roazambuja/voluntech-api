f2ed865048e4aefb90e9877569233e0c95361c97
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-18 23:48:03 -0300

    feat: multer middleware

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import registerValidator from "../helpers/validator";
 import { registerUser } from "../controllers/authController";
+import { upload } from "../middlewares/upload";
 
 const router = Router();
 
-export default [router.post("/", registerValidator, registerUser)];
+export default [router.post("/", upload.single("profilePicture"), registerValidator, registerUser)];
