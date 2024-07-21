d3116664d065652954ae2c2f3adfd56e34eae1d9
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-21 19:56:18 -0300

    feat: remove validator

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,7 @@
 import { Router } from "express";
-import registerValidator from "../helpers/validator";
 import UserController from "../controllers/userController";
 import { upload } from "../middlewares/upload";
 
 const router = Router();
 
-export default [
-  router.post("/", upload.single("profilePicture"), registerValidator, UserController.registerUser),
-];
+export default [router.post("/", upload.single("profilePicture"), UserController.registerUser)];
