23188cdc87ce19cde779e76539c9204eb2849109
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-24 18:53:53 -0300

    feat: validate cep

--- src/models/address.ts
+++ src/models/address.ts
@@ -2,7 +2,11 @@ import mongoose, { Schema } from "mongoose";
 import { Address } from "../entities/address";
 
 const addressSchema = new mongoose.Schema({
-  cep: { type: String, required: true },
+  cep: {
+    type: String,
+    required: true,
+    match: [/^\d{8}$/, "CEP inválido! Deve conter exatamente 8 caracteres numéricos."],
+  },
   city: { type: String, required: true },
   state: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
