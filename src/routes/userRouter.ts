cad57e4048eff2dc749f129030821684d44f9fe4
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-07-25 20:49:26 -0300

    feat: get user by id

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import UserController from "../controllers/userController";
 import { upload } from "../middlewares/upload";
+import checkToken from "../middlewares/token";
 
 const router = Router();
 
-export default [router.post("/", upload.single("profilePicture"), UserController.registerUser)];
+export default [
+  router.post("/", upload.single("profilePicture"), UserController.registerUser),
+  router.get("/:id", checkToken, UserController.getUser),
+];
