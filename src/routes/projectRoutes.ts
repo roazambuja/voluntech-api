5e3679055f6c6bdebcfff3bc0fd6e7bed0e44190
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-04 19:50:01 -0300

    feat: user controller and routes

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { upload } from "../middlewares/upload";
+import checkToken from "../middlewares/token";
+import ProjectController from "../controllers/projectController";
+
+const router = Router();
+
+export default [
+  router.post("/", upload.single("headerPicture"), checkToken, ProjectController.registerProject),
+];
