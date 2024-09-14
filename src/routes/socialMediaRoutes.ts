7c3406c4edcf0269216221d305b8dc6145c12861
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-14 18:26:43 -0300

    feat: update social media

--- src/routes/socialMediaRoutes.ts
+++ src/routes/socialMediaRoutes.ts
@@ -9,4 +9,5 @@ export default [
   router.post("/", checkToken, requireOrganization, SocialMediaController.registerSocialMedia),
   router.get("/:id", checkToken, SocialMediaController.getSocialMediaById),
   router.get("/user/:id", checkToken, SocialMediaController.getSocialMediaByUser),
+  router.put("/:id", checkToken, requireOrganization, SocialMediaController.updateSocialMedia),
 ];
