585190d7de964a11c731b9d6788e8b3b6f6ac6ea
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-09-24 17:53:28 -0300

    feat: register volunteering

---
+++ src/routes/volunteeringRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { requireOrganization } from "../middlewares/requireOrganization";
+import checkToken from "../middlewares/token";
+import VolunteeringController from "../controllers/volunteeringController";
+
+const router = Router();
+
+export default [
+  router.post("/", checkToken, requireOrganization, VolunteeringController.createVolunteering),
+];
