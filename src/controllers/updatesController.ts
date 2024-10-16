4afb67cbdffaf26a14da0ad48a650f9b67c79bc9
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-16 20:01:38 -0300

    feat: get posts that volunteers made in projects

--- src/controllers/updatesController.ts
+++ src/controllers/updatesController.ts
@@ -1,9 +1,10 @@
-import { Response } from "express";
+import { Response, Request } from "express";
 import FollowModel from "../models/follow";
 import ProjectModel from "../models/project";
 import VolunteeringModel from "../models/volunteering";
 import { AuthenticatedRequest } from "../middlewares/token";
 import PostModel from "../models/post";
+import OrganizationModel from "../models/organization";
 
 class UpdatesController {
   static getFollowedUpdates = async (req: AuthenticatedRequest, res: Response) => {
@@ -17,72 +18,117 @@ class UpdatesController {
         return res.status(400).json({ success: false, message: "Usuário não encontrado." });
       }
 
-      const followedOrganizations = await FollowModel.find({ user: user._id }).select(
-        "organization"
-      );
-      const followedOrganizationIds = followedOrganizations.map((follow) => follow.organization);
+      if (user.role === "Voluntário") {
+        const followedOrganizations = await FollowModel.find({ user: user._id }).select(
+          "organization"
+        );
+        const followedOrganizationIds = followedOrganizations.map((follow) => follow.organization);
 
-      const projects = await ProjectModel.find({
-        organization: { $in: followedOrganizationIds },
-      })
-        .populate("organization")
-        .lean();
+        const projects = await ProjectModel.find({
+          organization: { $in: followedOrganizationIds },
+        })
+          .populate("organization")
+          .lean();
 
-      const projectIds = projects.map((project) => project._id);
-      const volunteerings = await VolunteeringModel.find({
-        project: { $in: projectIds },
-      })
-        .populate({
-          path: "project",
-          populate: {
-            path: "organization",
+        const projectIds = projects.map((project) => project._id);
+
+        const volunteerings = await VolunteeringModel.find({
+          project: { $in: projectIds },
+        })
+          .populate({
+            path: "project",
+            populate: {
+              path: "organization",
+            },
+          })
+          .lean();
+
+        const posts = await PostModel.find({
+          project: { $in: projectIds },
+        })
+          .populate({
+            path: "project",
+          })
+          .populate({ path: "user" })
+          .lean();
+
+        const labeledProjects = projects.map((project) => ({
+          ...project,
+          type: "project",
+        }));
+
+        const labeledVolunteerings = volunteerings.map((volunteering) => ({
+          ...volunteering,
+          type: "volunteering",
+        }));
+
+        const labeledPosts = posts.map((post) => ({
+          ...post,
+          type: "post",
+        }));
+
+        const combinedUpdates = [...labeledProjects, ...labeledVolunteerings, ...labeledPosts];
+        combinedUpdates.sort((a, b) => (a._id < b._id ? 1 : -1));
+
+        const totalItems = combinedUpdates.length;
+        const paginatedUpdates = combinedUpdates.slice(
+          (pageNumber - 1) * pageLimit,
+          pageNumber * pageLimit
+        );
+
+        return res.status(200).json({
+          success: true,
+          data: paginatedUpdates,
+          pagination: {
+            totalItems,
+            totalPages: Math.ceil(totalItems / pageLimit),
+            currentPage: pageNumber,
+            pageLimit,
           },
+        });
+      } else if (user.role === "Organização") {
+        const userProjects = await ProjectModel.find({ organization: user._id }).select("_id");
+        const projectIds = userProjects.map((project) => project._id);
+
+        const postsByOthers = await PostModel.find({
+          project: { $in: projectIds },
+          user: { $ne: user._id },
         })
-        .lean();
+          .populate({
+            path: "project",
+            populate: {
+              path: "organization",
+            },
+          })
+          .populate({ path: "user" })
+          .lean();
 
-      const posts = await PostModel.find({
-        project: { $in: projectIds },
-      })
-        .populate({
-          path: "project",
-        })
-        .populate({ path: "user" })
-        .lean();
+        const labeledPosts = postsByOthers.map((post) => ({
+          ...post,
+          type: "post",
+        }));
 
-      const labeledProjects = projects.map((project) => ({
-        ...project,
-        type: "project",
-      }));
+        labeledPosts.sort((a, b) => (a._id < b._id ? 1 : -1));
 
-      const labeledVolunteerings = volunteerings.map((volunteering) => ({
-        ...volunteering,
-        type: "volunteering",
-      }));
+        const totalItems = labeledPosts.length;
+        const paginatedPosts = labeledPosts.slice(
+          (pageNumber - 1) * pageLimit,
+          pageNumber * pageLimit
+        );
 
-      const labeledPosts = posts.map((post) => ({
-        ...post,
-        type: "post",
-      }));
+        return res.status(200).json({
+          success: true,
+          data: paginatedPosts,
+          pagination: {
+            totalItems,
+            totalPages: Math.ceil(totalItems / pageLimit),
+            currentPage: pageNumber,
+            pageLimit,
+          },
+        });
+      }
 
-      const combinedUpdates = [...labeledProjects, ...labeledVolunteerings, ...labeledPosts];
-      combinedUpdates.sort((a, b) => (a._id < b._id ? 1 : -1));
-
-      const totalItems = combinedUpdates.length;
-      const paginatedUpdates = combinedUpdates.slice(
-        (pageNumber - 1) * pageLimit,
-        pageNumber * pageLimit
-      );
-
-      return res.status(200).json({
-        success: true,
-        data: paginatedUpdates,
-        pagination: {
-          totalItems,
-          totalPages: Math.ceil(totalItems / pageLimit),
-          currentPage: pageNumber,
-          pageLimit,
-        },
-      });
+      return res.status(400).json({ success: false, message: "Tipo de usuário inválido." });
     } catch (error: any) {
       return res.status(500).json({ success: false, message: error.message });
     }
