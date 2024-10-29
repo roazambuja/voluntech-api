7a9150e3d16ae3e8923fb44fb5decce35c81d423
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-28 21:38:51 -0300

    feat: message model and entity

---
+++ src/entities/message.ts
@@ -0,0 +1,9 @@
+import { ObjectId } from "mongoose";
+import { Organization, User } from "./user";
+
+export interface Message {
+  _id?: ObjectId;
+  content: string;
+  to: User | Organization;
+  from: User | Organization;
+}
---
+++ src/models/message.ts
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from "mongoose";
+import { Message } from "../entities/message";
+
+const messageSchema = new mongoose.Schema({
+  to: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  from: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+});
+
+const MessageModel = mongoose.model<Message>("Message", messageSchema);
+
+export default MessageModel;
