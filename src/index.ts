fc90dbb4dd8981a1a94b864b39974524bc35877d
Roberta Azambuja <robertaazambuja01@gmail.com>
2024-10-31 20:41:19 -0300

    feat: use socket

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import databaseConnection from "./database/connection";
 import router from "./routes/routes";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 
@@ -10,6 +12,15 @@ databaseConnection();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(
   cors({
@@ -23,9 +34,23 @@ app.use(express.json());
 app.use("/", router);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript Server with WebSocket");
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  socket.on("sendMessage", (payload) => {
+    const { from, to } = payload;
+    let fromId = from._id;
+    let toId = to._id;
+    const roomName = [fromId, toId].sort().join("-");
+    io.to(roomName).emit("newMessage", payload);
+  });
+
+  socket.on("joinRoom", (roomName) => {
+    socket.join(roomName);
+  });
+});
+
+httpServer.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
